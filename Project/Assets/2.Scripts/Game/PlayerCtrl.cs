7bffc4c8d6bfaa1a18154412a392c277d8139baf
cuptae <106813678+cuptae@users.noreply.github.com>
2025-01-21 01:39:35 +0900

    집작업

--- Project/Assets/2.Scripts/Game/PlayerCtrl.cs
+++ Project/Assets/2.Scripts/Game/PlayerCtrl.cs
@@ -5,37 +5,39 @@ using UnityEngine.Animations.Rigging;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    private Animator animator;
-    private CharacterController charCtrl;
-    private Camera mainCamera;
+    private Transform firePos;
     private Vector3 moveInput;
-    private bool isMove;
+    private float finalSpeed;
+    private bool isRun;
+
+    protected Camera mainCamera;
+    protected Animator animator;
+    protected CharacterController charCtrl;
+    protected bool isMove;
+
+    [HideInInspector]
+    public bool isFire;
     public float firingWalkSpeed = 3.0f;
     public float walkSpeed = 5.0f;
     public float runSpeed = 10.0f;
-    private float finalSpeed;
     public float jumpForce = 6.0f;
     public float rotationSpeed = 4.0f;
     public float gravity = 20.0f;
-    private bool isRun;
-    public bool isFire;
     public GameObject Weapon;
-    private Transform firePos;
     
-     [SerializeField]
-    private Rig aimRig;
 
-    private void Awake() {
+
+    protected virtual void Awake() {
         animator = GetComponentInChildren<Animator>();
         Debug.Assert(animator);
         charCtrl = GetComponent<CharacterController>();
         Debug.Assert(charCtrl);
         mainCamera = Camera.main;
     }
-    private void Update()
+    protected virtual void Update()
     {
         Move();
-        Shooting();
+        //Shooting();
     }
     void Move()
     {
@@ -45,7 +47,6 @@ public class PlayerCtrl : MonoBehaviour
         else{
             isRun = false;
         }
-        //finalSpeed = isRun?runSpeed:walkSpeed;
 
         if(isRun){
             finalSpeed = runSpeed;
@@ -56,8 +57,10 @@ public class PlayerCtrl : MonoBehaviour
         else{
             finalSpeed = walkSpeed;
         }
+
         float xAxis = Input.GetAxis("Horizontal");
 		float zAxis = Input.GetAxis("Vertical");
+
         moveInput = new Vector3(xAxis,0,zAxis);
 
         isMove = (moveInput.magnitude!=0)? true:false;
@@ -68,43 +71,34 @@ public class PlayerCtrl : MonoBehaviour
 
         // //percent는 애니메이션 블랜드 수치 run이 ture이면 1이고 false라면 0.5로 뛰는 애니메이션과 걷는 애니메이션 구분
         float percent = ((isRun) ? 1f : 0f) * moveInput.magnitude;
-        animator.SetFloat("Speed", percent, 0.1f, Time.deltaTime);
-        // 좌우 이동 값
-		animator.SetFloat("MoveX", xAxis);
-        // 전후 이동 값
-		animator.SetFloat("MoveZ", zAxis);
-
         Quaternion rotation = Quaternion.LookRotation(lookForward);
         transform.localRotation = Quaternion.Lerp(transform.localRotation,rotation,rotationSpeed*Time.deltaTime);
-        if(isMove){
-            animator.SetBool("Move",true);
-        }
-        else{
-            animator.SetBool("Move",false);
-        }
+
+
+        PlayerMoveAnim(isMove, xAxis, zAxis, percent);
+
+        //if(isMove){
+        //    animator.SetBool("Move",true);
+        //}
+        //else{
+        //    animator.SetBool("Move",false);
+        //}
         moveDir.y -= gravity * Time.deltaTime;
         charCtrl.Move(moveDir*finalSpeed*Time.deltaTime);
     }
 
-    void Shooting()
+
+    void PlayerMoveAnim(bool isMove,float xAxis,float zAxis,float percent)
     {
-        if(Input.GetMouseButton(0))
-        {
-            isFire = true;
-            SetRigWeight(1);
-            animator.SetBool("Fire",true);
-        }
-        else
-        {
-            isFire = false;
-            SetRigWeight(0);
-            animator.SetBool("Fire",false);
-        }
+        animator.SetFloat("Speed", percent, 0.1f, Time.deltaTime);
+        // 좌우 이동 값
+        animator.SetFloat("MoveX", xAxis);
+        // 전후 이동 값
+        animator.SetFloat("MoveZ", zAxis);
+        animator.SetBool("Move", isMove);
     }
 
-    void SetRigWeight(float weight)
-    {
-        aimRig.weight = weight;
-    }
+
+
 }
 
--- Project/Assets/2.Scripts/Game/WeaponRiple.cs
+++ Project/Assets/2.Scripts/Game/WeaponRiple.cs
@@ -4,49 +4,6 @@ using UnityEngine;
 
 public class WeaponRiple : MonoBehaviour
 {
-    Ray ray;
-    public Transform firePos;
-    public GameObject aimObj;
-
-    Camera mainCamera;
-
-    private void Awake() {
-        mainCamera = Camera.main;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        ray.direction = firePos.transform.forward;
-        Debug.DrawRay(firePos.position,ray.direction*30.0f,Color.green);
-        
-        if(Physics.Raycast(ray,out RaycastHit hit,Mathf.Infinity))
-        {
-            Debug.Log($"Hit {hit.collider.name}");
-            
-        }
 
 
-
-        // 마우스 위치를 기준으로 Ray 생성
-        Ray aimRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-
-        // 총구에서 Ray의 방향 설정
-        Vector3 direction = aimRay.direction.normalized;
-        Ray fireRay = new Ray(firePos.position, direction);
-
-        // Ray 디버그 (씬 뷰에서 확인 가능)
-        Debug.DrawRay(firePos.position, direction * 30f, Color.red);
-
-        // Raycast 처리
-        if (Physics.Raycast(fireRay, out RaycastHit hitInfo, Mathf.Infinity))
-        {
-            aimObj.transform.position = hitInfo.point+Vector3.up*0.5f;
-        }
-    }
 }
