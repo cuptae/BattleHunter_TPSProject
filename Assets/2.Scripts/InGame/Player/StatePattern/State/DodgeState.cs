89c3f39d54ab3401a98d95b555e03055cb5ff6cc
Cuptae <106813678+cuptae@users.noreply.github.com>
2025-03-19 19:14:21 +0900

    Feat: Dodge

--- Assets/2.Scripts/InGame/Player/StatePattern/State/DodgeState.cs
+++ Assets/2.Scripts/InGame/Player/StatePattern/State/DodgeState.cs
@@ -14,12 +14,28 @@ public class DodgeState : PlayerState
 
     public override void UpdateState() {}
 
-    public override void ExitState() {}
+    public override void ExitState()
+    {
+        player.rigid.velocity = Vector3.zero;        
+    }
     public override void FixedUpdateState(){}
 
     IEnumerator Dodge()
     {
         player.animator.SetTrigger("Dodge");
+        Vector3 dodgeDir = player.MoveDir();
+        if(player.MoveDir() == Vector3.zero)
+            dodgeDir = player.transform.forward;
+
+        if(player.MoveDir() != Vector3.zero)
+            player.transform.rotation = Quaternion.LookRotation(dodgeDir);
+        else
+            player.transform.rotation = Quaternion.LookRotation(dodgeDir);
+
+        if(!player.IsSlope())
+            player.rigid.AddForce(dodgeDir*player.dodgeForce,ForceMode.Impulse);
+        else
+            player.rigid.AddForce(Vector3.ProjectOnPlane(dodgeDir,player.groundNormal).normalized*player.dodgeForce,ForceMode.Impulse);
         yield return new WaitForSeconds(player.dodgeTime);
         player.ChangeState(new IdleState(player));
     }
