754cc991c433204250df6a26b5b51b8081174655
YUJJINCHO <nb410@naver.com>
2025-03-21 17:59:57 +0900

    20250321
    파티클 생성중

---
+++ Assets/2.Scripts/Manager/WeaponManager.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public static WeaponManager instance;
+
+    [Header("Bullet")]
+    [SerializeField]
+    private Transform bulletPoint;
+    [SerializeField]
+    private GameObject bulletObj;
+    [SerializeField]
+    private float maxShootDelay = 0.2f;
+        [SerializeField]
+    private float currentShootDelay = 0.2f;
+    
+
+    [Header("Weapon FX")]
+    [SerializeField]
+     private GameObject weaponFlashFX;
+    [SerializeField]
+     private Transform bulletCasePoint;
+    [SerializeField]
+    private GameObject bulletCaseFX;
+    [SerializeField]
+    private GameObject weaponClipPoint;
+    [SerializeField]
+    private GameObject weaponClipFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        currentShootDelay = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Shooting(Vector3 targetPosition)
+    {
+        currentShootDelay += Time.deltaTime;
+        if (currentShootDelay < maxShootDelay)
+        return ;
+
+        currentShootDelay = 0;
+   
+        Instantiate(weaponFlashFX,bulletPoint);
+        Instantiate(bulletCaseFX,bulletCasePoint);
+
+        Vector3 aim = (targetPosition - bulletPoint.position).normalized;
+        Instantiate(bulletObj, bulletPoint.position, quaternion.LookRotation(aim,Vector3.up));
+    }
+
+    public void ReroadClip()
+    {
+        Instantiate (weaponClipFX,weaponClipPoint);
+    }
+
+    private void Instantiate(GameObject weaponClipFX, GameObject weaponClipPoint)
+    {
+        throw new NotImplementedException();
+    }
+}
